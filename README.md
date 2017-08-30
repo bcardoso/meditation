11c3c56d15c5481c11ec3022b0486a659f1a4df8
bruno cardoso <cardoso.bc@gmail.com>
2017-08-30 01:55:07 -0300

    opção '-r' aceita formato TEMPO/INTERVALO

--- README.md
+++ README.md
@@ -5,5 +5,6 @@ timer para meditação e outras atividades.
 - `./meditation.sh`: padrão, intervalo de 20 minutos
 - `./meditation.sh 15`: intervalo de 15 minutos
 - `./meditation.sh 15 20 10`: um intervalo de 15, outro de 20 e um de 10
-- `./meditation.sh -r 30`: repete intervalos de 5m/30m indefinidamente
-- `./meditation.sh -set`: rotina de tempos pré-definida por $SET_TIMES
+- `./meditation.sh -r 23`: repete continuamente ciclos de 23m por 5m
+- `./meditation.sh -r 30/10`: idem, 30m por 10m
+- `./meditation.sh -set`: rotina de tempos pré-definida
--- meditation.sh
+++ meditation.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 ### meditation.sh 0.5 ~ timer para meditação
 ### bcardoso @ 2010-2017
 
@@ -7,7 +6,8 @@
 # ./meditation.sh           [padrão 20 minutos]
 # ./meditation.sh 15        [intervalo de 15 minutos]
 # ./meditation.sh 15 20 10  [um intervalo de 15, outro de 20 e um de 10]
-# ./meditation.sh -r 30     [repete intervalos de 5m/30m indefinidamente]
+# ./meditation.sh -r 23     [repete continuamente ciclos de 23m por 5m]
+# ./meditation.sh -r 30/10  [idem, 30m por 10m]
 # ./meditation.sh -set      [rotina de tempos pré-definida]
 
 ### CHANGELOG
@@ -28,19 +28,19 @@ BASEDIR=~/bin/meditation/
 # som que será tocado ao fim de cada intervalo
 # bigbowl.mp3: http://www.freesound.org/samplesViewSingle.php?id=132
 # singing-bell: https://www.freesound.org/people/ryancacophony/sounds/202017/
-#SOM=$BASEDIR/bigbowl.mp3
-#SOM=$BASEDIR/singing-bell-hit2.mp3
+#SOM=$BASEDIR/sound/bigbowl.mp3
+#SOM=$BASEDIR/sound/singing-bell-hit2.mp3
 SOM=$BASEDIR/sound/singing-bell-hit2-normalizado.mp3
 
-# player que tocará o som: mplayer // alternative: "/usr/bin/cvlc --play-and-exit"
+# player para o som
+# alternatives: "/usr/bin/cvlc --play-and-exit", "mpv --really-quiet"
 PLAYER="mplayer -really-quiet -volume 90"
 
 # intervalo padrão em minutos
-DEFAULT=20
+DEFAULT=23
 
 # rotina pré-definida para opção -set
-SET_TIMES="5 25 5 55 5" # ~1h30 focus work (20% of 8h/work-day)
-#SET_TIMES="1 1 2 3 5 8 13 21 34" # fibonacci times
+SET_TIMES="25 5 55 5" # ~1h30 focus work (20% of 8h/work-day)
 
 
 #============================================================================#
@@ -65,11 +65,11 @@ timer () {
 		check_time $num
 
 		# faz a contagem do tempo
-		echo -e "\n[$(date +%H:%M)] Tempo: $TEMPO minutos..."
+		echo -e "[$(date +%H:%M)] $TEMPO min..."
 		sleep "$TEMPO"m
 	
 		# toca o sino ao final
-		$PLAYER $SOM 2> /dev/null
+		($PLAYER $SOM & 2> /dev/null)
 	done
 }
 
@@ -82,29 +82,38 @@ case $1 in
 	
 	-h) # AJUDA, mostra principais opções
 		head -12 $0
-		grep ") \#" $0 | tr "\#" "\t"
+		grep ") \#" $0 | sed -e 's/\t/\ /g;s/)\ \#/\t/g'
 		;;
 
 	
-	-r) # REPETE um intervalo '$2' a cada 5m, indefinidamente. ex: -r 25
-		check_time $2
-		echo -e "\nSET DE PRÁTICA: 5m/${TEMPO}m (oo)"
-		while ((1)) ; do
-			echo -ne "\n>> INTERVALO"
-			timer 5
-			echo -ne "\n>> PRÁTICA"
-			timer $2
+	-r) # REPETE um ciclo X/Y. ex: -r 23/5
+		shift
+		if [[ $1 == *"/"* ]]; then
+			X=$(echo $1 | cut -d"/" -f1)
+			Y=$(echo $1 | cut -d"/" -f2)
+		else
+			X=$1
+			Y=5
+		fi
+
+		# inicia a repetição do set
+		echo -e "\nSET: ${X}m/${Y}m (oo)"
+		while :; do
+			echo -ne "\n>> PRÁTICA "
+			timer $X
+			echo -ne "\n>> <PAUSA> "
+			timer $Y
 		done
 		;;
 
 
-	-set) # SET pré-definido de intervalos ($SET_TIMES)
-		echo -e "\nSET DE PRÁTICA: $SET_TIMES"
+	-set) # SET de intervalos pré-definido ($SET_TIMES)
+		echo -e "\nSET: $SET_TIMES"
 		timer $SET_TIMES
 		;;
 
 
-	*) # INTERVALO modo padrão ($DEFAULT) ou N argumentos
+	*) # INTERVALO padrão ($DEFAULT) ou N argumentos
 		if [ -z $1 ] ; then
 			timer $DEFAULT
 		else
